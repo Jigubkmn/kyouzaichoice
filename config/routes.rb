9285b792d53379433af1d920b4a3447e6b95cdeb
bkmn31519 <bkmn.31519@gmail.com>
2024-07-30 01:33:42 +0900

    ログインログアウト機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'top#index'
   resources :users, only: %i[new create]
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
   # Defines the root path route ("/")
   # root "articles#index"
 end
