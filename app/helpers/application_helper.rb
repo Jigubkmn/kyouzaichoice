5d0dea98269ea8b5220691048ad6ac16ed01ba49
bkmn31519 <bkmn.31519@gmail.com>
2024-07-31 00:11:16 +0900

    フラッシュメッセージ実装

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def flash_background_color(type)
+    case type.to_sym
+    when :success  then "bg-flash-messages-green"
+    when :danger   then "bg-flash-messages-red"
+    when :error  then "bg-flash-messages-yellow"
+    else "bg-gray-500"
+    end
+  end
 end
