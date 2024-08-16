63953207defec78152c4609f091141b20292f6d6
bkmn31519 <bkmn.31519@gmail.com>
2024-08-16 21:35:37 +0900

    追加済み教材画面を修正

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,8 @@ Rails.application.routes.draw do
   resources :qualifications, only: %i[new index create edit update destroy]
   resources :materials, only: %i[new index create edit update destroy] do
     collection { get :search } # 教材検索用
-    collection { get :like } # いいねした教材一覧表示用
+    collection { get :already_registered } # 「追加済み教材」表示用
+    collection { get :like } # 「いいねした教材」表示用
     # material_evaluationコントローラーを用意していないため、material内に記述している
     resources :comments, only: %i[create edit destroy], shallow: true
   end
