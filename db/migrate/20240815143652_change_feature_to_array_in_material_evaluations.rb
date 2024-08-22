5de125e9010612c93a9da20cae74a98abbf9eb10
bkmn31519 <bkmn.31519@gmail.com>
2024-08-22 20:33:24 +0900

    マイグレーションファイル修正

--- db/migrate/20240815143652_change_feature_to_array_in_material_evaluations.rb
+++ db/migrate/20240815143652_change_feature_to_array_in_material_evaluations.rb
@@ -1,5 +1,9 @@
 class ChangeFeatureToArrayInMaterialEvaluations < ActiveRecord::Migration[7.0]
-  def change
-    change_column :material_evaluations, :feature, :string, array: true, default: [], using: "string_to_array(feature, ',')"
+  def up
+    change_column :material_evaluations, :evaluation, 'float USING evaluation::float'
+  end
+
+  def down
+    change_column :material_evaluations, :evaluation, :integer
   end
 end
