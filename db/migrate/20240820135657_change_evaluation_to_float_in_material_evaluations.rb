1631a630a8b22914c20a2cbca56f8a7f55ae796f
bkmn31519 <bkmn.31519@gmail.com>
2024-08-22 20:46:20 +0900

    マイグレーションファイル

--- db/migrate/20240820135657_change_evaluation_to_float_in_material_evaluations.rb
+++ db/migrate/20240820135657_change_evaluation_to_float_in_material_evaluations.rb
@@ -1,6 +1,10 @@
 class ChangeEvaluationToFloatInMaterialEvaluations < ActiveRecord::Migration[7.0]
-  def change
-    change_column :material_evaluations, :evaluation, :float
+  def up
+    change_column :material_evaluations, :evaluation, 'float USING evaluation::float'
+  end
+
+  def down
+    change_column :material_evaluations, :evaluation, :integer
   end
 end
  
