f1b25b5b148da7f468c88cbc6c6e5b1b1720cad1
bkmn31519 <bkmn.31519@gmail.com>
2024-09-02 20:52:03 +0900

    微調整

--- db/schema.rb
+++ db/schema.rb
@@ -46,17 +46,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_01_173434) do
     t.index ["user_id"], name: "index_material_evaluations_on_user_id"
   end
 
-  create_table "material_evalutions", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "material_id"
-    t.float "evaluation"
-    t.string "feature", default: ""
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["material_id"], name: "index_material_evalutions_on_material_id"
-    t.index ["user_id"], name: "index_material_evalutions_on_user_id"
-  end
-
   create_table "materials", force: :cascade do |t|
     t.string "title", null: false
     t.string "image_link", null: false
@@ -93,7 +82,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_01_173434) do
   add_foreign_key "likes", "users"
   add_foreign_key "material_evaluations", "materials"
   add_foreign_key "material_evaluations", "users"
-  add_foreign_key "material_evalutions", "materials"
-  add_foreign_key "material_evalutions", "users"
   add_foreign_key "qualifications", "users"
 end
