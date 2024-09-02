dcd247eb6189164aca6a922018acecd0c7a6baa1
bkmn31519 <bkmn.31519@gmail.com>
2024-09-02 22:09:00 +0900

    render設定変更

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,7 +5,4 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-# bundle exec rails db:migrate
-
-# マイグレーションの実行
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
+bundle exec rails db:migrate
