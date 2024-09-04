779f94ffa0757e1e851cdf07c1871f982e83a51d
bkmn31519 <bkmn.31519@gmail.com>
2024-09-04 22:01:26 +0900

    renderデプロイ修正

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Myapp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
     # renderデプロイに必要
-    config.hosts << 'qualificationtake.onrender.com'
+    config.hosts << 'www.kyouzaichoice.com'
     # Configuration for the application, engines, and railties goes here.
 
     # These settings can be overridden in specific environments using the files
