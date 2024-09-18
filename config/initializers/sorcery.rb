cf0086da5a48a65565f82596e8b18b14adb1a456
bkmn31519 <bkmn.31519@gmail.com>
2024-09-18 23:28:25 +0900

    google.callback_url修正

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -84,7 +84,6 @@ Rails.application.config.sorcery.configure do |config|
   config.external_providers = %i[google]
   config.google.key = Rails.application.credentials.dig(:google, :google_client_id)
   config.google.secret = Rails.application.credentials.dig(:google, :google_client_secret)
-  # config.google.callback_url = "http://localhost:3000/oauth/callback?provider=google"
   config.google.callback_url = Settings.sorcery[:google_callback_url]
   config.google.user_info_mapping = { email: "email", name: "name" }
   # You can change it by your local ca_file. i.e. '/etc/pki/tls/certs/ca-bundle.crt'
