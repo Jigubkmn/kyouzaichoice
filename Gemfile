b386ec4de34f11bd54734f207e4274f5dc2c6dea
bkmn31519 <bkmn.31519@gmail.com>
2024-09-30 21:59:03 +0900

    パスワードリセット本番環境に対応

--- Gemfile
+++ Gemfile
@@ -69,6 +69,8 @@ gem 'config'
 # Use Sass to process CSS
 # gem "sassc-rails"
 
+gem 'dotenv-rails'
+
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,9 +69,9 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    domain: 'gmail.com',
-    user_name: Rails.application.credentials.gmail[:user_name],
-    password: Rails.application.credentials.gmail[:password],
+    domain: 'www.kyouzaichoice.com/',
+    user_name: ENV['GMAIL_USERNAME'],
+    password: ENV['GMAIL_PASSWORD'],
     authentication: 'plain',
     enable_starttls_auto: true
   }
