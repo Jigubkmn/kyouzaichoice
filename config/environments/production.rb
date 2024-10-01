b4df0b4160952ad495a24fd460b7577c6c7c2101
bkmn31519 <bkmn.31519@gmail.com>
2024-10-01 21:36:09 +0900

    Google認証URL変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,5 +103,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.hosts << "qualificationtake.onrender.com"
+  config.hosts << "www.kyouzaichoice.com"
 end
