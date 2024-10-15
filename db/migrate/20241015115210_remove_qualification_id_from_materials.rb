8c1388ae737d65ac65d8be7a47df1abd59a74d9c
bkmn31519 <bkmn.31519@gmail.com>
2024-10-15 21:11:41 +0900

    データベース修正

--- db/migrate/20241015115210_remove_qualification_id_from_materials.rb
+++
@@ -1,5 +0,0 @@
-class RemoveQualificationIdFromMaterials < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :materials, :qualification_id, :bigint
-  end
-end
--- db/migrate/20241015115803_remove_material_id_from_qualification.rb
+++
@@ -1,5 +0,0 @@
-class RemoveMaterialIdFromQualification < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :qualifications, :material_id, :bigint
-  end
-end
