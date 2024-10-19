6841d1316a82aa2d6787261a884b6ad1e6f63f93
bkmn31519 <bkmn.31519@gmail.com>
2024-10-19 22:18:55 +0900

    JavaScriptファイル修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,3 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
-import "./year_acquired/application";
-import "./material_description/application";
-import "./show_hide";
-import "./material_form";
--- app/javascript/material_description/application.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const modal = document.getElementById('my_modal_5');
-  document.querySelector('button[onclick="my_modal_5.showModal()"]').onclick = function() {
-    modal.showModal();
-  };
-});
