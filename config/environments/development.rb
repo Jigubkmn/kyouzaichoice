13781ceb7b62e044c5fd91dba1c475b760b2a117
bkmn31519 <bkmn.31519@gmail.com>
2024-10-27 20:52:36 +0900

    Git Hub Actionエラー対応

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,6 +71,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "127.0.0.1" 
   config.hosts << "172.27.0.3" 
   config.hosts << "172.27.0.4" 
   config.hosts << "172.30.0.3" 
