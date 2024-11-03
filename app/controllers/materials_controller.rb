6c87fa819407523147cb7ae28f27d4e903b947eb
bkmn31519 <bkmn.31519@gmail.com>
2024-11-03 12:12:28 +0900

    オートコンプリートに資格名を追加

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -46,7 +46,7 @@ class MaterialsController < ApplicationController
 
   # オートコンプリート
   def index_autocomplete
-    @materials = Material.where('title like ?', "%#{params[:q]}%")
+    @materials = Material.where('title Like ? OR qualification Like ?', "%#{params[:q]}%", "%#{params[:q]}%")
     render partial: 'materials/index_autocomplete'
   end
 
